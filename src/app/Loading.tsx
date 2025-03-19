1fd3257051d30db31073c974802024de1c479f98
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2025-03-19 16:45:02 -0300

    update

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Loading = () => {
     return (
         <div className='h-screen w-full flex items-center justify-center border'>
-            <Image src={"/loader.gif"} alt='loader' height={100} width={100}/>
+            <Image unoptimized priority src={"/loader.gif"} alt='loader' height={100} width={100}/>
         </div>
     )
 }
